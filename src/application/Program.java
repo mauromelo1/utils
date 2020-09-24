069eade2fab9c9334cd5d297e886d74cbf61c670
Mauro Melo <mauromelo1@gmail.com>
2020-09-24 14:35:28 -0300

    Classe Utilitarios

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Date;
+import java.util.Random;
+
+import helper.Utils;
+
+public class Program {
+	
+public static void main(String[] args) {
+		
+		Random gerador = new Random();
+		
+		Date data = new Date();
+		//System.out.println(data);		
+		System.out.println(Utils.dateParaString(data));
+		
+		Double preco = 10000 * gerador.nextDouble(); 
+		//System.out.println(preco);
+		System.out.println(Utils.doubleParaString(preco));
+				
+	}
+
+}
